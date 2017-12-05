9420b83f556816bdd93c4cb4011060a75e547ce3
Nhiokh <samuel.abiassi@gmail.com>
2017-12-05 11:48:43 +0100

    Created component.js, scss and css

---
+++ component.js
@@ -0,0 +1,16 @@
+//  @flow
+
+import React from 'react';
+import './component.css';
+
+type Props = {};
+
+export default class appComponent extends React.Component<Props> {
+  render(){
+    return (
+      <div>
+        <p>Simple React App</p>
+      </div>
+    )
+  }
+}
